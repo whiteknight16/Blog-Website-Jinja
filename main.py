4efa608c6ff74c61ab02c80f8065a49e51ac408f
Harsh Pandey <harshp6421@gmail.com>
2023-05-31 20:43:15 +0530

    Updated the blog website

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template
 import requests
+import datetime
+
+year=datetime.datetime.now().year
 
 
-response = requests.get('https://api.npoint.io/4964104e0ae06aa7ec8b')
-all_blogs = response.json()
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template("index.html", posts=all_blogs)
+def get_all_posts():
+    return render_template("index.html", all_posts=posts,year=year)
 
 
-@app.route('/post/<int:num>')
-def my_posts(num):
-    current_blog = None
-    print(num)
-    for blog in all_blogs:
-        if blog['id'] == num:
-            current_blog = blog
-    return render_template('post.html', post=current_blog)
+@app.route("/about")
+def about():
+    return render_template("about.html",year=year)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html",year=year)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post,year=year)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
